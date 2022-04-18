5cf43d9de1abb2f2efef36ee403a76d6c7e55ab2
astrid21 <44526477+astrid21@users.noreply.github.com>
2022-04-18 19:04:55 -0300

    Add files via upload

---
+++ Primer Parcial/AppHerramientasParcial1 (para el alumno)/AppParcialI/Models/Alumno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppParcialI.Models
+{
+    public class Alumno
+    {
+        [Required(ErrorMessage = "El {0} es de ingreso obligatorio.")]
+        [Range(100,999)]
+        public int Legajo { set; get; }
+
+        [Required(ErrorMessage = "El {0} es de ingreso obligatorio.")]
+        [MaxLength(10, ErrorMessage = "El {0} tiene un máximo de {1} caracteres.")]
+        public string Apellido { set; get; }
+
+        [Required(ErrorMessage = "El {0} es de ingreso obligatorio.")]
+        [Range(0, 10, ErrorMessage = "El {0} es debe estar entre {1} y {2}.")]
+        public int Promedio { set; get; }        
+    }
+}
